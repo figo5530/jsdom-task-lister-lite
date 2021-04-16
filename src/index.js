bf971613d78260b8275eea11b68b8439743719b9
figo5530 <figooooo0221@gmail.com>
2021-04-16 20:49:43 +0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementById("create-task-form").addEventListener("submit", (event) => {
+    event.preventDefault()
+    document.getElementById("tasks").innerHTML += `<li>${document.getElementById("new-task-description").value}</li>`
+    event.target.reset()
+  })
 });
